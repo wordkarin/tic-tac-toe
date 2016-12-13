a1e56c71b6b39316a57f97b87bd1f5bd89208328
Yuri Nakashima <yuri310@gmail.com>
2016-12-13 15:14:37 -0800

    positive and negative tests for Player play function are passing

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -1,1 +1,28 @@
 import Player from 'player';
+
+describe('Player', function(){
+  describe('play', function(){
+    it('should pass a position between 0 and 8', function(){
+      var player = new Player();
+
+      expect(player.play(1)).toEqual(1);
+
+      expect(player.play(0)).toEqual(0);
+
+      expect(player.play(8)).toEqual(8);
+
+      expect(function() {
+        player.play("cat");
+      }).toThrow(new Error('Input must be an integer between 0 and 8'));
+
+      expect(function() {
+        player.play(9);
+      }).toThrow(new Error('Input must be an integer between 0 and 8'));
+
+      expect(function() {
+        player.play(-1);
+      }).toThrow(new Error('Input must be an integer between 0 and 8'));
+
+    });
+  });
+});
--- src/player.js
+++ src/player.js
@@ -1,7 +1,17 @@
 import Board from 'board';
 
 var Player = function() {
-//if Game assigns attributes, do we need to put anything here for Player?
+
+};
+
+Player.prototype.play = function (position) {
+  if (!(Number.isInteger(position))) {
+    throw new Error('Input must be an integer between 0 and 8');
+  } else if ( position < 0|| position > 8) {
+      throw new Error('Input must be an integer between 0 and 8');
+  } else {
+    return position;
+  }
 };
 
 
