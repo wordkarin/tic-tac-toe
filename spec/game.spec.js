8793346cef7f830361d8266be1e7e217b751f73f
Karin Kubischta <karin.kubischta@gmail.com>
2016-12-13 14:00:21 -0800

    game constructor tests creation of players and board

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -1,10 +1,18 @@
 import Game from 'game';
+import Player from 'player';
+import Board from 'board';
 
 describe('Game', function(){
   describe('constructor', function() {
     it('should make a new game', function(){
       var game1 = new Game();
       expect(game1 instanceof Game).toBeTruthy();
+
+      expect(game1.player1 instanceof Player).toBeTruthy();
+
+      expect(game1.player2 instanceof Player).toBeTruthy();
+
+      expect(game1.board instanceof Board).toBeTruthy(); 
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -3,7 +3,11 @@ import Board from 'board';
 
 
 var Game = function () {
+  // for each game, we make two players
+  this.player1 = new Player();
+  this.player2 = new Player();
 
+  this.board = new Board(); 
 };
 
 
