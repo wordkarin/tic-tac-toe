2464cbfa5b9e3e4c3ee4707fc626a35057261867
Yuri Nakashima <yuri310@gmail.com>
2016-12-19 11:15:11 -0800

    resolved stack overflow on gameview, click event on play again working

--- src/app.js
+++ src/app.js
@@ -2,15 +2,15 @@ import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
 
-import Game from 'app/models/game';
-import Board from 'app/models/board';
-import BoardView from 'app/views/board_view';
+// import Game from 'app/models/game';
+// import Board from 'app/models/board';
+// import BoardView from 'app/views/board_view';
 import GameView from 'app/views/game_view';
 
 $(document).ready(function(){
-  var boardview = new BoardView({
-    el: '.board'
+  var gameview = new GameView({
+    el: '#game'
   });
 
-  boardview.render();
+  gameview.render();
 });
--- src/app/views/game_view.js
+++ src/app/views/game_view.js
@@ -1,17 +1,30 @@
 import Backbone from 'backbone';
-import Game from 'app/models/game';
-import Board from 'app/models/board';
+// import Game from 'app/models/game';
+// import Board from 'app/models/board';
+import BoardView from 'app/views/board_view';
 
 
 const GameView = Backbone.View.extend({
   initialize: function(options) {
+    var board = new BoardView({
+      el: '.board'
+    });
 
+    board.render();
   },
 
   render: function() {
 
     return this;
   },
+
+  events: {
+    'click button': 'restartGame'
+  },
+
+  restartGame: function(event) {
+    console.log('restartGame called');
+  }
 });
 
 export default GameView;
